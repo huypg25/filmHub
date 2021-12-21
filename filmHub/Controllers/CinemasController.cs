d47faa7857fb74af7abf2152dced7cc7ee638b9a
k1pp <k1pp@DESKTOP-E7RCRLS>
2021-12-21 22:19:49 +0700

    add Controller

---
+++ filmHub/Controllers/CinemasController.cs
@@ -0,0 +1,18 @@
+﻿using filmHub.data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace filmHub.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ filmHub/Controllers/ProducersController.cs
@@ -0,0 +1,20 @@
+﻿using filmHub.data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace filmHub.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
