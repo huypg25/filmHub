de00dca38cad6cc57a3fa841542ad752e0e8dd31
k1pp <k1pp@DESKTOP-E7RCRLS>
2021-12-21 17:42:40 +0700

    add model

---
+++ filmHub/Data/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace filmHub.data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Cartoon,
+        Horror
+    }
+}
--- filmHub/Program.cs
+++ filmHub/Program.cs
@@ -1,7 +1,16 @@
+using filmHub.data;
+using filmHub.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//DbContext configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
@@ -23,5 +32,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed database
+AppDbInitializer.Seed(app);
 
 app.Run();
